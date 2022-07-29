564b22fb37836ad8a7e439b70d8e8bc17d9f8e5d
daiyadeguchi <daiyadeguchi@gmail.com>
2022-07-30 07:59:20 +0900

    Update CategoryViewController to show fetched Firestore entry

---
+++ ToDo or NotToDo/Model/Items.swift
@@ -0,0 +1,13 @@
+//
+//  Items.swift
+//  ToDo or NotToDo
+//
+//  Created by Daiya Deguchi on 2022/07/30.
+//
+
+import Foundation
+
+struct Items: Codable {
+    let category: String
+    let owner: String
+}
